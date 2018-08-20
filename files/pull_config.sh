6e227299f01243ac9d2b51c5590d44ccf5feb2b0
Sofiane Medjkoune <sofiane@medjkoune.fr>
2018-08-20 08:46:56 +0200

    Should be able to handle all cases

--- files/pull_config.sh
+++ files/pull_config.sh
@@ -6,11 +6,18 @@ cd $(dirname $0)
 
 [[ ! -d .git ]] && git init
 
-ORIGIN=$(git remote -v | grep 'origin' | grep '(fetch)' | awk '{print $2}')
-[[ "$ORIGIN" != $1 ]] && git remote remove origin && git remote add -f origin $1 && git reset --hard origin/master
-
 BEFORE=$(git log | head -1)
+
+ORIGIN=$(git remote -v | grep 'origin' | grep '(fetch)' | awk '{print $2}')
+if [[ "$ORIGIN" != $1 ]]
+then
+    git remote remove origin
+    git remote add -f origin $1
+    git reset --hard origin/master
+fi
+
 git pull origin master
+
 AFTER=$(git log | head -1)
 [[ "$BEFORE" != "$AFTER" ]] && sudo systemctl restart homeassistant
 
