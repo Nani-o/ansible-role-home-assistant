57475537c905703161efb8f160484f8b6278d8d5
Sofiane Medjkoune <sofiane@medjkoune.fr>
2018-08-20 08:01:06 +0200

    Add sudo capabilities to the user and auto restart in the pulling script

--- files/pull_config.sh
+++ files/pull_config.sh
@@ -9,6 +9,7 @@ cd $(dirname $0)
 ORIGIN=$(git remote -v | grep 'origin' | grep '(fetch)' | awk '{print $2}')
 [[ "$ORIGIN" != $1 ]] && git remote add -f origin $1 && git reset --hard origin/master
 
-git pull origin master
+PULL="$(git pull origin master)"
+[[ "$PULL" != "Already up to date." ]] && sudo systemctl restart homeassistant
 
 exit 0
