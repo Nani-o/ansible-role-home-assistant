525ea584cad6d0002fb5438976f06a14caafd67c
Sofiane Medjkoune <sofiane@medjkoune.fr>
2018-08-20 08:39:20 +0200

    ZzzZzZzZz

--- files/pull_config.sh
+++ files/pull_config.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 [[ ! -d .git ]] && git init
 
 ORIGIN=$(git remote -v | grep 'origin' | grep '(fetch)' | awk '{print $2}')
-[[ "$ORIGIN" != $1 ]] && git remote remove origin master && git remote add -f origin $1 && git reset --hard origin/master
+[[ "$ORIGIN" != $1 ]] && git remote remove origin && git remote add -f origin $1 && git reset --hard origin/master
 
 BEFORE=$(git log | head -1)
 git pull origin master
